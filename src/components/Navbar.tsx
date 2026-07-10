0c6a9fb60211519e5c2331b65aaae9d72bfacf1e
Subash Sabari <subashsabari636@gmail.com>
2026-07-10 13:39:25 +0530

    fix: push navbar logo to absolute left edge by removing max-w-7xl container constraint

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -50,7 +50,7 @@ export default function Navbar() {
             : 'bg-transparent py-6'
         }`}
       >
-        <div className="max-w-7xl mx-auto px-6 flex justify-between items-center">
+        <div className="w-full px-6 flex justify-between items-center">
           {/* Logo */}
           <Link href="/" className="flex items-center group">
             <img
